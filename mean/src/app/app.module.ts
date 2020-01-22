e0a170988b99ac7b7bf6a2f7923f7b49382cc653
Suresh Koochana <suresh.koochana@fortunapix.com>
2020-01-22 18:53:30 +0530

    updated code for form fields

--- mean/src/app/app.module.ts
+++ mean/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './post/post-create/post-create.component';
+import { MatInputModule,
+         MatCardModule,
+         MatButtonModule,
+         MatToolbarModule,
+         MatExpansionModule,
+         } from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mean/src/app/post/post-create/post-create.component.ts
+++ mean/src/app/post/post-create/post-create.component.ts
@@ -1,9 +1,23 @@
-import { Component } from "@angular/core";
-
+import { Component,EventEmitter,Output } from "@angular/core";
+import { Post } from "../post.model";
+import { NgForm } from "@angular/forms";
 @Component({
-    selector:'app-post-create',
-    template:'./post-create.component.ts'
+   selector:'app-post-create',
+   templateUrl:'./post-create.component.html',
+   styleUrls:['./post-create.component.css']
 })
 export class PostCreateComponent{
-
+   enteredContent = '';
+   enteredTitle = '';
+   @Output() postCreated = new EventEmitter<Post>();
+   onAddPost(form:NgForm){
+      if(form.invalid){
+         return;
+      }
+      const post:Post ={
+         title:form.value.title,
+         content:form.value.content
+      };
+      this.postCreated.emit(post);
+   }
 }
