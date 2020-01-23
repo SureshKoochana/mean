4a85b93809956d429a3027bf5710549214ffc0b8
Suresh Koochana <suresh.koochana@fortunapix.com>
2020-01-23 12:27:07 +0530

    basic angular app

--- mean/src/app/app.module.ts
+++ mean/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { MatInputModule,
          MatCardModule,
          MatButtonModule,
@@ -12,7 +12,7 @@ import { MatInputModule,
          MatExpansionModule,
          } from '@angular/material';
 import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './post/post-list/post-list.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
